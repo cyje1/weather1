5c3b1ea2102ab1f467a39cad65389bb7661841e9
DESKTOP-MRTMBQP\ckddy <ckddyd28@gmail.com>
2024-01-29 19:10:48 +0900

    commit

---
+++ src/main/java/zerobase/weather/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package zerobase.weather.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "날씨 일기 프로젝트 :)",
+                description = "날씨 일기를 CRUD 할 수 있는 벡엔드 API 입니다.",
+                version = "v1"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+}
+
